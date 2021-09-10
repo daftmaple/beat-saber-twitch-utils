d09604303f0407a58413a406e72ffdbdd0a57e19
daftmaple <ubuntumaple@gmail.com>
2021-09-10 19:55:55 +1000

    feat(tts): added currently-playing speech skip on filter
    Using the currentSpeech state provided by speech hook, if the speech needs to be filtered,
    cancelCurrentSpeech is invoked and it will set the currentSpeech state to null by onend utterance
    callback.

--- src/components/tts/hook/linker.ts
+++ src/components/tts/hook/linker.ts
@@ -19,37 +19,38 @@ interface Props {
 export const useLinker = (props: Props) => {
   const { enabled, client } = props;
   const [messageQueue, messageQueueActions] = useMessageQueue();
-  const speech = useSpeech();
+  const {
+    currentSpeech,
+    setCurrentSpeech,
+    setEndCallback,
+    cancelCurrentSpeech,
+  } = useSpeech();
 
+  /**
+   * Triggers speech. Speech is only triggered when:
+   * - TTS is enabled on the enabled props (currently as a button)
+   * - Queue has a message
+   * - There is no currently running speech
+   *
+   * This hook runs as a callback since it is passed to speech hook
+   * which then invoked on message end
+   */
   const triggerSpeech = useCallback(() => {
-    /**
-     * Speech is only triggered when:
-     * - TTS is enabled on the enabled props (currently as a button)
-     * - Queue has a message
-     * - There is no currently running speech
-     *
-     * This hook runs as a callback since it is passed to speech hook
-     * which then invoked on message end
-     */
-    if (
-      enabled &&
-      messageQueueActions.countMessages() > 0 &&
-      !speech.currentSpeech
-    ) {
+    if (enabled && messageQueueActions.countMessages() > 0 && !currentSpeech) {
       const messageToSpeak = messageQueueActions.popMessage();
       if (messageToSpeak) {
-        speech.setCurrentSpeech({
-          message: messageToSpeak.message,
-          username: messageToSpeak.username,
-        });
+        setCurrentSpeech(messageToSpeak);
       }
     }
-  }, [enabled, messageQueueActions, speech]);
+  }, [currentSpeech, enabled, messageQueueActions, setCurrentSpeech]);
 
   useEffect(() => {
-    speech.setEndCallback(triggerSpeech);
-  }, [speech, triggerSpeech]);
+    setEndCallback(triggerSpeech);
+  }, [setEndCallback, triggerSpeech]);
 
+  /**
+   * Adds message to queue
+   */
   const messageHandler = useCallback<CallbackHandler<Message>>(
     (payload) => {
       // If payload exists and message doesn't start with command trigger, speak the message
@@ -64,31 +65,47 @@ export const useLinker = (props: Props) => {
     [messageQueue],
   );
 
+  /**
+   * Clears queue based on username and stops message if username is matching
+   */
   const userFilterHandler = useCallback<CallbackHandler<Ban>>(
     (payload) => {
       if (payload) {
         messageQueue.clearUserMessage(payload.username);
+        if (currentSpeech?.username === payload.username) {
+          cancelCurrentSpeech();
+        }
       }
     },
-    [messageQueue],
+    [currentSpeech, messageQueue, cancelCurrentSpeech],
   );
 
+  /**
+   * Clears queue and empties currently spoken speech
+   */
   const clearChatHandler = useCallback<CallbackHandler<ClearChat>>(
     (payload) => {
       if (payload) {
         messageQueue.clearMessages();
+        cancelCurrentSpeech();
       }
     },
-    [messageQueue],
+    [messageQueue, cancelCurrentSpeech],
   );
 
+  /**
+   * Deletes message from queue and stops message from playing if id is matching
+   */
   const messageDeletedHandler = useCallback<CallbackHandler<MessageDeleted>>(
     (payload) => {
       if (payload) {
         messageQueue.deleteMessage(payload[`msg-id`]);
+        if (currentSpeech?.[`msg-id`] === payload[`msg-id`]) {
+          cancelCurrentSpeech();
+        }
       }
     },
-    [messageQueue],
+    [currentSpeech, messageQueue, cancelCurrentSpeech],
   );
 
   useEffect(() => {
