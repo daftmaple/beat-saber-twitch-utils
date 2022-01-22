076b489eb85ebe472aa0660ee5c4d30e4baf6508
daftmaple <ubuntumaple@gmail.com>
2022-01-22 17:07:22 +1100

    fix(beatgif): redirect to 404 if no image exist from slug

--- src/pages/gif/[slug].tsx
+++ src/pages/gif/[slug].tsx
@@ -2,12 +2,14 @@ import { useRouter } from 'next/router';
 import { ReactElement } from 'react';
 
 import { BeatGifComponent } from '~/components/beatGif';
+import { images } from '~/config/images';
 import { routeHandler, queryHandler } from '~/utils/bs-websocket';
 
-const Gif = (): ReactElement => {
+const Gif = (): ReactElement | Promise<boolean> => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { query } = useRouter();
+  const router = useRouter();
 
+  const { query } = router;
   const { slug } = query;
 
   const {
@@ -18,11 +20,17 @@ const Gif = (): ReactElement => {
 
   const gifName = routeHandler(slug);
 
-  return (
-    <>
-      <BeatGifComponent debug={debug} gifName={gifName} ip={ip} port={port} />
-    </>
-  );
+  const imageResult = images.filter((i) => i.gifName === gifName);
+
+  if (imageResult.length !== 0) {
+    return (
+      <>
+        <BeatGifComponent debug={debug} gifName={gifName} ip={ip} port={port} />
+      </>
+    );
+  }
+
+  return router.push('/404');
 };
 
 export default Gif;
